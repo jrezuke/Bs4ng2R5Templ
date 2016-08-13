64a46f944259d2be03411bfbd6f1b444f544482d
jrezuke <j.rezuke@verizon.net>
2016-08-13 17:02:46 -0400

    added css and libs to index

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,6 @@ import { MdListModule} from '@angular2-material/list'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>Angular 2 - Bootstrap 4</h1>'
 })
 export class AppComponent { }
