7080b5cf5227372f4fa7dd5269fe88bc82cfaf19
jrezuke <j.rezuke@verizon.net>
2016-08-13 11:38:50 -0400

    initial

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { MdListModule} from '@angular2-material/list'
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
